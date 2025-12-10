bfa89f795ed65271aff413d72adbadc0637aa13b
Way-Too-Easy <git.fornino@gmail.com>
2025-12-11 00:13:44 +0400

    website

---
+++ scripts/show-virtualenv_tag.sh
@@ -0,0 +1,9 @@
+# add the following to your ~/.zshrc file fot pyenv tags in your terminal
+# note: ohmyzsh might delete the contents of your file,
+# so you will have to add this after installing ohmyzsh
+
+# if command -v 1>/dev/null 1>&1; then
+#    eval "$(pyenv init -)"
+# export PATH=/usr/local/scripts:$PATH
+# eval "$(pyenv init -)"
+# eval "$(pyenv init virtualenv-init -)"
