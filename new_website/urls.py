bac64add714b159b38c2eb89cd4d09ab1837df12
Way-Too-Easy <git.fornino@gmail.com>
2025-12-14 23:39:42 +0400

    login - logout

--- new_website/urls.py
+++ new_website/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView 
 
 from . import views
 
@@ -27,5 +28,6 @@ urlpatterns = [
     path('contact', views.contact, name='contact'),
 
     #django auth
-    path('accounts/login', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login')
+    path('accounts/login', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
