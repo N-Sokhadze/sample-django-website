ed493f9f27fa62e3487db92812bc9730a6394cfb
Way-Too-Easy <git.fornino@gmail.com>
2025-12-17 18:45:49 +0400

    user(models.py) - migrations

--- new_website/apps/accounts/models.py
+++ new_website/apps/accounts/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class UserPersona(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    normalized_name = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length = 200)
+    
+    def __str__(self):
+        return self.name
+         
+
 # Create your models here.
 class UserProfile(models.Model):
     #owner
