0ad67812a924c29c4dc1b3c5f84e8664481c9209
Way-Too-EAsy <git.fornino@gmail.com>
2026-04-16 18:14:34 +0400

    from wsl

--- new_website/views.py
+++ new_website/views.py
@@ -1,6 +1,11 @@
+# python manage.py runserverfrom django.views.generic.base import TemplateView
+
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/profile.html'
+
+
+    
