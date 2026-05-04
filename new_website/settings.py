1bfd8c0aa4c9c3d8081158612aca4c762a391545
Way-Too-EAsy <git.fornino@gmail.com>
2026-05-04 17:52:15 +0400

    frontend 10 min - 6th episode

--- new_website/settings.py
+++ new_website/settings.py
@@ -133,7 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
+#django auth settings
 LOGIN_URL = 'accounts:login'
 LOGIN_REDIRECT_URL = 'accounts:profile' #'public:index'
 LOGOUT_REDIRECT_URL = 'public:index'
