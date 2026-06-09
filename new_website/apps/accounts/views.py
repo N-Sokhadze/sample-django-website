a05502db59ba11fcb31adb6306da7f72a20fbee4
Way-Too-EAsy <git.fornino@gmail.com>
2026-06-09 23:11:53 +0400

    ContactForm

--- new_website/apps/accounts/views.py
+++ new_website/apps/accounts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest, HttpResponse
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
 def index(request):
     print(request.user)
     return render(request, 'index.html')
@@ -18,3 +19,5 @@ def contact(request):
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/profile.html'
+
+
--- new_website/apps/contact/views.py
+++ new_website/apps/contact/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+from .forms import ContactForm
+
 def contact(request: HttpRequest) -> HttpResponse:
-    return render(request, 'contact.html')
+    if request.method == 'GET':
+        form = ContactForm()
+        # return render(request, 'contact.html')
+    
+    else:
+        raise NotImplementedError
+    return render(request, "contact.html", {'form': form})
+
+    
