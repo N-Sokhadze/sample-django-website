99fc97056d6269a53096f75ed361235e5c5acf78
Way-Too-EAsy <git.fornino@gmail.com>
2026-06-26 18:05:27 +0400

    email validator

--- new_website/apps/contact/forms.py
+++ new_website/apps/contact/forms.py
@@ -1,6 +1,20 @@
 from django import forms
+from email_validator import validate_email, EmailNotValidError
 
 class ContactForm(forms.Form):
     name = forms.CharField(required=True, max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Name"}))
+    
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': "Email"}))
+    
+    # _______email validator________
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        try:
+            validate_email(email, check_deliverability=True)
+        except EmailNotValidError:
+            raise forms.ValidationError('Please enter a valid email address.')
+        return email
+    # _______________________________
+    
     message = forms.CharField(required=True, max_length=1000, widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': "Message", 'rows': 5}))
